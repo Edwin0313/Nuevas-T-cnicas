7e8a7e5580e7c2df0def45a2059bda4811947557
Edwin0313 <32930905+Edwin0313@users.noreply.github.com>
2017-11-21 09:26:59 -0500

    Add files via upload

---
+++ ArticuloMD.php
@@ -0,0 +1,62 @@
+<?php
+include("Conexion.php");
+
+class ArticuloMD
+{
+
+    function insertar($articulo) 
+	{
+		$conec=conectar();
+		
+		$cod_art=$articulo->getCodigo();
+		$nombre==$articulo->getNombre();
+		$tipo=$articulo->getTipo();
+		$estado=$articulo->getEstado();
+		$valor=$articulo->getValor();
+		
+		$query= "insert into `articulo`(`COD_ARTICULO`, `NOMBRE`, `TIPO`, `ESTADO`, `VALOR`)
+		VALUES('".$cod_art."','".$nombre."','".$tipo."','".$estado."','".$valor."')";
+	
+		$result=mysqli_query($conec,$query);
+
+		if($result)
+			echo "Artículo insertado exitosamente";
+		else
+			die(mysqli_error($conec));
+		
+	}
+    
+	function consultar($articulo) 
+	{
+		$conec=conectar();
+		
+		$cod_art=$articulo->getCodigo();
+		
+		$query= "SELECT `COD_ARTICULO`, `NOMBRE`, `TIPO`, `ESTADO`, `VALOR` FROM `articulo` WHERE `COD_ARTICULO`='".$cod_art."'";
+	
+		$result=mysqli_query($conec,$query);
+
+		if($result)
+			echo "Consulta exitosa";
+		else
+			die(mysqli_error($conec));
+		
+	}
+	function eliminar($articulo) 
+	{
+		$conec=conectar();
+		
+		$cod_art=$articulo->getCodigo();
+		
+		$query= "DELETE FROM `articulo` WHERE `COD_ARTICULO`='".$cod_art."'";
+	
+		$result=mysqli_query($conec,$query);
+
+		if($result)
+			echo "Eliminación exitosa";
+		else
+			die(mysqli_error($conec));
+		
+	}
+}
+?>
